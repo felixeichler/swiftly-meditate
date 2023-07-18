2f1dfacac0e150051e2fa81fa73a09cf716d8df6
Felix <felixeichler@users.noreply.github.com>
2023-07-18 10:10:51 +0200

    Initial Commit

---
+++ meditation app/meditation_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  meditation_appApp.swift
+//  meditation app
+//
+//  Created by Felix Eichler on 18.07.23.
+//
+
+import SwiftUI
+
+@main
+struct meditation_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
